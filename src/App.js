a96e0fcdf265ed4bed87c66efb14ec5e7af73f16
hahays <diggabeat@gmail.com>
2022-12-09 20:57:27 +0300

    20/57

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
 import Navibar from "./containers/Navibar/Navibar";
-import MainPage from "./containers/Pages/MainPage/MainPage";
-import WhyWeACool from "./containers/Pages/WhyWeACool/WhyWeACool";
-import CotactUs from "./containers/Pages/ContactUs/CotactUs";
-import AboutUs from "./containers/Pages/AboutUs/AboutUs";
-import Team from "./containers/Pages/Team/Team";
-import Answers from "./containers/Pages/Answers/Answers";
-import Gifts from "./containers/Pages/Gifts/Gifts";
-import Vacancies from "./containers/Pages/Vacancies/Vacancies";
-import Comments from "./containers/Pages/Comments/Comments";
-import ElseComments from "./containers/Pages/ElseComments/ElseComments";
-import Calculator from "./containers/Pages/Calculator/Calculator";
-import TesLuna from "./containers/Pages/TestLuna/TesLuna";
 import Footer from "./containers/Pages/Footer/Footer";
 import { Route, Routes } from "react-router-dom";
 import FirstPage from "./containers/Pages/FirstPage";
 import SecondPage from "./containers/Pages/SecondPage";
 import ThirdPage from "./containers/Pages/ThirdPage";
+import "normalize.css";
 
 function App() {
   return (
@@ -32,5 +21,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
